e1445e10b49ce79e2186c727890bc97a084add12
Sayaka Yoshii <yoshii@sayakamac.local>
2018-01-03 12:28:11 +0900

    チーム名をapp.iniにもつ

--- eagles/inc/header.php
+++ eagles/inc/header.php
@@ -1,9 +1,14 @@
+<?php 
+global $iniParam;
+//adminユーザ
+$title = $iniParam['title'];
+?>
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="robots" content="noindex,nofollow">
-		<title>イーグルス</title>
+		<title><?php echo $title?></title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
 		<link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
